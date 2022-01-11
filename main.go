5cdf104414d277ba6f7aed7536d5ba10691e704a
tompston <tompston@protonmail.com>
2022-01-11 14:44:06 +0200

    added check for config file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gomakeme/codegen"
 	"gomakeme/input"
 	"log"
@@ -10,18 +11,22 @@ import (
 
 func main() {
 
-	conf, err := input.NewConfig("gomakeme.yml")
-	if err != nil {
-		log.Fatal(err)
+	// if the config file exists
+	if codegen.PathExists("./gomakeme.yml") {
+
+		conf, err := input.NewConfig("gomakeme.yml")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// generate init project
+		codegen.GenerateInitProject(*conf)
+		// generate the modules specified in the .yml file
+		codegen.GenerateModulesFromConfig(conf)
+		// keep the modules specified in the yaml file in sync with the modules that are imported to the router
+		codegen.UpdateModuleImporter(*conf)
+
+	} else {
+		fmt.Println(" ---ERROR---   Could not find gomakeme.yml")
 	}
-
-	// generate init project
-	codegen.GenerateInitProject(*conf)
-
-	// generate the modules specified in the .yml file
-	codegen.GenerateModulesFromConfig(conf)
-
-	// keep the modules specified in the yaml file in sync with the modules that are imported to the router
-	codegen.UpdateModuleImporter(*conf)
-
 }
